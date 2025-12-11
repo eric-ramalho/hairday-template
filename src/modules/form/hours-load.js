3c834e8da70b71d9870a7a73ca2021bc1b8f859b
Eric Ramalho <ericramalho@hotmail.com>
2025-12-11 10:25:28 -0300

    Renderizando os horários

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from "dayjs";
 
 import { openingHours } from "../../utils/opening-hours.js";
+const hours = document.getElementById("hours");
+
 export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
     const [scheduleHour, __] = hour.split(":");
@@ -8,6 +10,15 @@ export function hoursLoad({ date }) {
     const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs());
 
     // Define se o horario está disponivel
-    return { hour, availabre: isHourPast };
+    return { hour, available: isHourPast };
+  });
+
+  // Renderizar os horarios
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li");
+    li.classList.add("hour");
+    li.classList.add(available ? "hour-available" : "hour-unavailable");
+    li.textContent = hour;
+    hours.append(li);
   });
 }
