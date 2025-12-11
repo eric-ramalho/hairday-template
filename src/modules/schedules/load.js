80c46ba3ad1c92b14978f40593ff4982047ae503
Eric Ramalho <ericramalho@hotmail.com>
2025-12-11 13:26:18 -0300

    Buscando os agendamentos na API

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,16 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
 
 //seleciona o input de data
 const selectDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   //obtem a data do input
   const date = selectDate.value;
+
+  //buscar na API os agendamentos
+  const dailySchedules = await scheduleFetchByDay({ date });
+  console.log(dailySchedules);
 
   hoursLoad({ date });
 }
